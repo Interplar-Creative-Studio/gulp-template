7547d3f75ab362bca76adcb149a66b97158cbca6
Edward Haussauer <edhaussauer@gmail.com>
2021-11-30 17:32:56 +0300

    advanced dev mode

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,19 @@ const pug2html = require('./tasks/pug2html')
 const pug2html_prod = require('./tasks/production/pug2html')
 
 const images = require('./tasks/images')
+const images_prod = require('./tasks/production/images')
+
 const scripts = require('./tasks/scripts')
 const fonts = require('./tasks/fonts')
 const static = require('./tasks/static')
+
 const serve = require('./tasks/serve')
+const serve_prod = require('./tasks/production/serve')
 
 const clean = require('./tasks/clean')
 
 let dev = series(pug2html, scss2css, scripts, fonts, static, images)
-let prod = series(pug2html_prod, scss2css_prod, scripts, fonts, static, images)
+let prod = series(pug2html_prod, scss2css_prod, scripts, fonts, static, images_prod)
 
 task('styles:dev', series(scss2css))
 task('styles:prod', series(scss2css_prod))
@@ -23,7 +27,7 @@ task('styles:prod', series(scss2css_prod))
 task('pages:dev', series(pug2html))
 task('pages:prod', series(pug2html_prod))
 
-task('imagemin', series(images))
+task('imagemin', series(images_prod))
 task('scripts', series(scripts))
 task('fonts', series(fonts))
 task('static', series(static))
@@ -33,6 +37,7 @@ task('compile:prod', prod)
 
 task('clean', series(clean))
 
-task('serve', series(serve))
+task('serve:dev', series(serve))
+task('serve:prod', series(serve_prod))
 
 task('start', series(dev, serve))
--- tasks/production/serve.js
+++ tasks/production/serve.js
@@ -1,10 +1,11 @@
 const { watch, series } = require('gulp');
 
 const images = require('./images');
-const static = require('./static');
 const scss2css = require('./scss2css');
-const scripts = require('./scripts');
 const pug2html = require('./pug2html');
+
+const static = require('../static');
+const scripts = require('../scripts');
 
 const server = require('browser-sync').create();
 
